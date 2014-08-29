1d1c31442ed7858f40313323ea2835cd6fbc3a4b
gamerdave <david@wieser.at>
2014-08-29 20:31:30 +0200

    edit ai and spawn

---
+++ Assets/Source/Spawn.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawn : MonoBehaviour {
+
+	public Transform spawnObject;
+
+	public float interval = 0f;
+	private float timer;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+		if (interval > 0) {
+			timer += Time.deltaTime;
+		}
+
+	}
+}
