04f0c5dcf1a84b7b112f7bec1157b05abb16be82
David Wieser <david@wieser.at>
2014-08-29 21:59:44 +0200

    #5

--- Assets/Source/Gun2D.cs
+++ Assets/Source/Gun2D.cs
@@ -12,24 +12,30 @@ public class Gun2D : MonoBehaviour {
 	public int ammo;
 	public Text ammoDisplay;
 
+	public float minRotation = 0, maxRotation = 360;
+
 	void Start () {
 	
 	}
 
 	public void TryFire () {
-		if (delay <= 0) {
+		if (delay <= 0 && ammo > 0) {
 			delay = 1/fireRate;
+			ammo --;
 			Fire ();
 		}
 	}
 
 	public void Fire () {
 	
-		Instantiate (bullet, transform.position, transform.rotation);
+//		Instantiate (bullet, transform.position, transform.rotation);
+		Debug.Log ("Fire");
 
 	}
 	
 	void Update () {
+
+		ammoDisplay.text = "Ammo: "+ammo;
 	
 		if (delay > 0) {
 			delay -= Time.deltaTime;
@@ -39,7 +45,10 @@ public class Gun2D : MonoBehaviour {
 			ammo += 3;
 		}
 
-		transform.eulerAngles = new Vector3(0,0,-Mathf.Atan2(Input.mousePosition.x - Screen.width/2 - transform.position.x, Input.mousePosition.y - Screen.height/2 - transform.position.y)*Mathf.Rad2Deg + 90);
+		float newRotation = -Mathf.Atan2(Input.mousePosition.x - Screen.width/2 - transform.position.x, Input.mousePosition.y - Screen.height/2 - transform.position.y)*Mathf.Rad2Deg + 90;
+		if (newRotation > minRotation-90 && newRotation < maxRotation-90) {
+			transform.localEulerAngles = new Vector3(0,0,newRotation);
+		}
 
 		if (Input.GetMouseButton (0)) {
 			TryFire ();
