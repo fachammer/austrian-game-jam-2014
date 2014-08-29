d0d68773ccdb582be494252459f4d201aba3dd61
Fabian Achammer <fabian.achammer@gmail.com>
2014-08-29 22:59:36 +0200

    added DoorGenerator

---
+++ Assets/Source/EnumerableExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class EnumerableExtensions
+{
+    public static IEnumerable<T> Do<T>(this IEnumerable<T> source, Action<T> action) {
+        foreach (var element in source)
+            action(element);
+
+        return source;
+    }
+}
