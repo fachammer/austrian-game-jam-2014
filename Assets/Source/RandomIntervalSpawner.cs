2e31bb799288d223d968021a34c03ae42f2096b6
Fabian Achammer <fabian.achammer@gmail.com>
2014-08-30 00:01:20 +0200

    added RandomIntervalSpawner

---
+++ Assets/Source/RandomIntervalSpawner.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class RandomIntervalSpawner : MonoBehaviour
+{
+    public GameObject objectToSpawn;
+    public float minSpawnInterval;
+    public float maxSpawnInterval;
+
+    private float currentInterval;
+    private float timer;
+
+    private static float CalculateNewInterval(float minInterval, float maxInterval) {
+        return Random.Range(minInterval, maxInterval);
+    }
+
+    private void Start() {
+        currentInterval = CalculateNewInterval(minSpawnInterval, maxSpawnInterval);
+    }
+
+    private void Update() {
+        timer += Time.deltaTime;
+
+        if (timer >= currentInterval) {
+            timer = 0f;
+            currentInterval = CalculateNewInterval(minSpawnInterval, maxSpawnInterval);
+            Instantiate(objectToSpawn, transform.position, transform.rotation);
+        }
+    }
+}
