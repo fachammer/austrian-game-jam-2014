bec4277cbc8f50fd24d99d19239736b88b7bce1a
David Wieser <david@wieser.at>
2014-08-29 21:24:58 +0200

    #3

---
+++ Assets/Source/Gun2D.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gun2D : MonoBehaviour {
+
+	public float fireRate = 3;
+	private float delay;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+		transform.eulerAngles = new Vector3(0,0,Mathf.Atan2(Input.mousePosition.x - Screen.width/2 - transform.position.x, Input.mousePosition.y - Screen.height/2 - transform.position.y)*Mathf.Rad2Deg);
+
+	}
+}
