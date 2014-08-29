39e98809026292c51b733ab1a42f96ebe23dd465
David Wieser <david@wieser.at>
2014-08-29 21:44:22 +0200

    #4

--- Assets/Source/Gun2D.cs
+++ Assets/Source/Gun2D.cs
@@ -1,18 +1,49 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Gun2D : MonoBehaviour {
 
 	public float fireRate = 3;
 	private float delay;
 
+	public Transform bullet;
+
+	public int ammo;
+	public Text ammoDisplay;
+
 	void Start () {
 	
+	}
+
+	public void TryFire () {
+		if (delay <= 0) {
+			delay = 1/fireRate;
+			Fire ();
+		}
+	}
+
+	public void Fire () {
+	
+		Instantiate (bullet, transform.position, transform.rotation);
+
 	}
 	
 	void Update () {
 	
-		transform.eulerAngles = new Vector3(0,0,Mathf.Atan2(Input.mousePosition.x - Screen.width/2 - transform.position.x, Input.mousePosition.y - Screen.height/2 - transform.position.y)*Mathf.Rad2Deg);
+		if (delay > 0) {
+			delay -= Time.deltaTime;
+		}
+
+		if (Input.GetKeyDown (KeyCode.R)) {
+			ammo += 3;
+		}
+
+		transform.eulerAngles = new Vector3(0,0,-Mathf.Atan2(Input.mousePosition.x - Screen.width/2 - transform.position.x, Input.mousePosition.y - Screen.height/2 - transform.position.y)*Mathf.Rad2Deg + 90);
+
+		if (Input.GetMouseButton (0)) {
+			TryFire ();
+		}
 
 	}
 }
