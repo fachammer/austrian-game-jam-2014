d50a3318f38253d50c029b92c048ed9788f2ab17
David Wieser <david@wieser.at>
2014-08-29 23:13:58 +0200

    updated player controller

---
+++ Assets/Source/CameraZoom.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraZoom : MonoBehaviour {
+
+	public enum Controlls {ScrollWhell, PlusMinus, Both}
+
+	public Controlls controlls;
+
+	public float min = 5, max = 50;
+
+	private float current;
+
+	void Start () {
+	
+		if (camera.isOrthoGraphic) {
+			current = camera.orthographicSize;
+		} else {
+			current = -camera.transform.position.z;
+		}
+
+	}
+	
+	void Update () {
+	
+		if (camera.isOrthoGraphic) {
+			camera.orthographicSize = current;
+		} else {
+			transform.position = new Vector3(transform.position.x, transform.position.y, -current);
+		}
+
+		if (controlls == Controlls.PlusMinus) {
+			if (Input.GetKeyDown(KeyCode.Plus) && current < max) {
+				current ++;
+			} else if (Input.GetKeyDown(KeyCode.Minus) && current > min) {
+				current --;
+			}
+		} else if (controlls == Controlls.ScrollWhell) {
+			if (Input.GetAxis ("Mouse ScrollWheel") < 0 && current < max) {
+				current ++;
+			} else if (Input.GetAxis ("Mouse ScrollWheel") > 0 && current > min) {
+				current --;
+			}
+		} else {
+			if (Input.GetKeyDown(KeyCode.Plus) && current < max) {
+				current ++;
+			} else if (Input.GetKeyDown(KeyCode.Minus) && current > min) {
+				current --;
+			}
+			if (Input.GetAxis ("Mouse ScrollWheel") < 0 && current < max) {
+				current ++;
+			} else if (Input.GetAxis ("Mouse ScrollWheel") > 0 && current > min) {
+				current --;
+			}
+		}
+
+	}
+}
--- Assets/Source/PlayerController.cs
+++ Assets/Source/PlayerController.cs
@@ -6,46 +6,67 @@ public class PlayerController : MonoBehaviour {
 
 	public float speed = 10f;
 
-	public float timeBetweenShots = 500.0f;
+	// Movement Settings
+	public float moveForce = 365f;
+	public float maxSpeed = 5f;	
+	public float jumpForce = 1000f;	
 
-	//private Player player;
-	private float lastTimeShot;
+	[HideInInspector]
+	public bool jump = false;
+	[HideInInspector]
+	public bool facingLeft = false;
 
-	private bool turnLeft = false;
+	private Transform groundCheck;
+	private bool grounded = false;	
 
-	void Start()
-    {
-		//player = GameObject.FindGameObjectWithTag(Tags.Player).GetComponent<Player>();
-		lastTimeShot = Time.time;
+	void Awake () {
+		groundCheck = transform.Find("GroundCheck");
 	}
 
-	void Update()
-    {
+	void Update () {
+
+		grounded = Physics2D.Linecast(transform.position, groundCheck.position); 
+
+		if (Input.GetButtonDown("Jump") && grounded) {
+			jump = true;
+		}
+
+	}
+
+	void FixedUpdate () {
 
 		Vector2 velocity = rigidbody2D.velocity;
 
-		if(Input.GetKey(KeyCode.Space) && Time.time - lastTimeShot > timeBetweenShots){
-			lastTimeShot = Time.time;
-			//player.SendMessage("Shoot");
+		float horizontalInput = Input.GetAxis("Horizontal");
+
+		if (horizontalInput * rigidbody2D.velocity.x < maxSpeed) {
+			rigidbody2D.AddForce (Vector2.right * horizontalInput * moveForce);
 		}
 
-		if (Input.GetAxis ("Horizontal") > 0) {
-			if (turnLeft) {
-				turnLeft = false;
-				transform.localScale = new Vector3(1,1,1);
-			}
-			velocity.x += speed;
-		} else if (Input.GetAxis("Horizontal") < 0) {
-			if (!turnLeft) {
-				turnLeft = true;
-				transform.localScale = new Vector3(-1,1,1);
-			}
-			velocity.x -= speed;
+		if (Mathf.Abs (rigidbody2D.velocity.x) > maxSpeed) {
+			rigidbody2D.velocity = new Vector2 (Mathf.Sign (rigidbody2D.velocity.x) * maxSpeed, rigidbody2D.velocity.y);
 		}
 
-		rigidbody2D.velocity = velocity;
+		if(horizontalInput > 0 && !facingLeft)
+			Flip();
+		else if(horizontalInput < 0 && facingLeft)
+			Flip();
+
+		if (jump) {
+			rigidbody2D.AddForce(new Vector2(0f, jumpForce));
+			jump = false;
+		}
 
 	}
+
+	void Flip () {
+		facingLeft = !facingLeft;
+
+		Vector3 newScale = transform.localScale;
+		newScale.x *= -1;
+		transform.localScale = newScale;
+	}
+
 }
 
 
