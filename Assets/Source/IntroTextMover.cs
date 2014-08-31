d092f73b97c70c30138399d0319bc52e81d9e1d6
ibeda86 <peter.kainrad@gmx.at>
2014-08-31 12:23:35 +0200

    Add intro Scene.

---
+++ Assets/Source/IntroTextMover.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntroTextMover : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector3.back * Time.deltaTime);
+		transform.Translate(Vector3.up * Time.deltaTime/10, Space.World);
+	}
+}
